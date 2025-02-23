5e0fc2add16b17115d5f1896b3b57dd59d1060d6
R4ll0d <thanabatsr45@gmail.com>
2025-02-23 14:46:59 +0700

    set up for cloud run

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func init() {
 }
 
 func main() {
-	port := os.Getenv("APP_PORT") // Cloud Run ใช้ตัวแปร PORT
+	port := os.Getenv("PORT") // Cloud Run ใช้ตัวแปร PORT
 	if port == "" {
 		port = "8080" // ตั้งค่าเริ่มต้นเป็น 8080
 	}
@@ -55,8 +55,7 @@ func main() {
 }
 
 func initConfig() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	if err := godotenv.Load(); err != nil {
+		log.Println("Warning: .env file not found, using environment variables")
 	}
 }
